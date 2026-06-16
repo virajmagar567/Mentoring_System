1d13650c7a1df140f71cef4eea4c01e52f28d628
Viraj <your-github-email@example.com>
2026-06-16 22:32:16 +0530

    Add WhiteNoise middleware and set DEBUG=False for production

--- Mentoring_System/settings.py
+++ Mentoring_System/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o6se-&a!ps7-lin@hmmy+l&)twj27xzndgd@x9b1#zlj^j$chv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
